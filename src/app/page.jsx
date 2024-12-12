d088e29d541c01cec7f098bb05f98e3fb8896295
max <obregonjefferson2@gmail.com>
2024-12-12 18:34:21 -0500

    refactorizacion codigo para funcionamiento

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,29 +1,30 @@
 import TaskCard from "@/components/taskCard";
 import { prisma } from "@/libs/prisma"
 
-  async function loadTask(){
-    //LISTAR LAS TAREAS - TENGO DOS FORMAS:
-    //1) obteniendo de la DB mediante PRISMA
-    //2) hacinedo una peticion HTTPS /API/TASKS  uso de fetch
-    return await prisma.task.findMany();
-  }
+async function loadTask() {
+  //LISTAR LAS TAREAS - TENGO DOS FORMAS:
+  //1) obteniendo de la DB mediante PRISMA
+  //2) hacinedo una peticion HTTPS /API/TASKS  uso de fetch
+  return await prisma.task.findMany();
+}
 
-  export const dynamic='force-dynamic'
-  
+export const dynamic = 'force-dynamic'
+
 async function HomePage() {
-  const tasks= await loadTask();
-  
+  const tasks = await loadTask();
+
+
   return <section className=" container mx-auto">
     <h1 className="text-3xl font-bold text-gray-300 text-center pt-3">ALL TASKS HERE - EDIT AND DELETE</h1>
-  <div className=" grid grid-cols-3 gap-3">
-    {
-    tasks.map((task)=>(
-      <TaskCard task={task}  key={task.id}/>      
-    ))
-    }
-  </div>
+    <div className=" grid grid-cols-3 gap-3">
+      {
+        tasks.map((task) => (
+          <TaskCard task={task} key={task.id} />
+        ))
+      }
+    </div>
   </section>
 
- 
+
 }
 export default HomePage
--- src/components/taskCard.jsx
+++ src/components/taskCard.jsx
@@ -2,14 +2,14 @@
 import React from 'react'
 import { useRouter } from 'next/navigation'
 
-function TaskCard({task}) {
-   const router= useRouter(); 
-   
+function TaskCard({ task }) {
+  const router = useRouter();
+
   return (
     <div>
       <div className=" bg-slate-900 p-3 rounded-xl mt-10 ml-2 mr-2 hover:bg-slate-700 hover:cursor-pointer"
-        onClick={()=>{
-            router.push('/tasks/edit/'+ task.id)
+        onClick={() => {
+          router.push('/tasks/edit/' + task.id)
         }}
       >
         <h1 className=" font-bold text-2xl mb-2">{task.id} .{task.title}</h1>
